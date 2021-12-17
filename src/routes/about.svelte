cd878e9a0dc48ecfc48d9c9c0f976c8e9c0a1629
Leo <lcheung@swellbottle.com>
2021-12-17 13:04:13 -0500

    add nav to __layout.svelte

---
+++ src/routes/about.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+    <title>About Svelte Kit Pokedex</title>
+</svelte:head>
+
+<h1>About</h1>
+<p>This is a really cool Svelte Kit demo that uses the Pokemon API to build a Pokedex</p>
+
+<style>
+	h1 {
+		color:red;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,15 @@
 <script>
-    
+    import Nav from "../components/nav.svelte"
 </script>
+
+<svelte:head>
+    <title>Svelte Kit Pokedex</title>
+</svelte:head>
+
 <h1>Svelte Kit Pokedex</h1>
+
+<style>
+	h1 {
+		color: rgb(89, 210, 49);
+	}
+</style>
