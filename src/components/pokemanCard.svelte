af74745b43d1d9b100ffffb502829d23ffbd2c88
Leo <lcheung@swellbottle.com>
2021-12-17 13:39:40 -0500

    search and filter pokemon

--- src/components/pokemanCard.svelte
+++ src/components/pokemanCard.svelte
@@ -4,6 +4,6 @@
 </script>
 
 <a class="p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center" href={`/pokeman/${pokeman.id}`}>
-    <img src={pokeman.image} alt={pokeman.name} />
-    <h2>{pokeman.id}. {pokeman.name}</h2>
+    <img class="h-40 w-40" src={pokeman.image} alt={pokeman.name} />
+    <h2 class="uppercase text-2xl">{pokeman.id}. {pokeman.name}</h2>
 </a>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,21 @@
 	import PokemanCard from '../components/pokemanCard.svelte';
 
 	// console.log($pokemon); // add "$" make it reactive
+
+    let searchTerm = "";
+    let filteredPokemon = [];
+
+    // make reactive
+    $:{
+        console.log(searchTerm);
+        if(searchTerm) {
+            // search and filter the pokemon
+            filteredPokemon = $pokemon.filter(pokeman => pokeman.name.toLowerCase().includes(searchTerm.toLowerCase()));
+        } else {
+            // reset the filter and show all pokemons
+            filteredPokemon = [...$pokemon]
+        }
+    }
 </script>
 
 <svelte:head>
@@ -11,8 +26,10 @@
 
 <h1 class="text-4xl text-center my-8 uppercase">Svelte Kit Pokedex</h1>
 
+<input class="w-full rounded-md text-lg p-4 border-2 border-gray-200" type="text" bind:value={searchTerm} placeholder="Search Pokemon">
+
 <div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
-{#each $pokemon as pokeman}
+{#each filteredPokemon as pokeman}
 	<PokemanCard {pokeman} />
 {/each}
 </div>
